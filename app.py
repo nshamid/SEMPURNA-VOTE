3c0945bef94da646ee05fa3f07ef49f47e3561e3
Nabilah Shamid <nblhshamid08@gmail.com>
2026-01-08 13:45:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,7 @@ VOTERS_PATH = "data/voters.csv"
 
 kandidat = [
     {"nama": "Syifa", "foto": "images/Kandidat1.jpg"},
-    {"nama": "Yuwa", "foto": "images/Kandidat2.jpg"},
     {"nama": "Era", "foto": "images/Kandidat3.jpg"},
-    {"nama": "Fivi", "foto": "images/Kandidat4.jpg"},
     {"nama": "Atika", "foto": "images/Kandidat5.jpg"},
     {"nama": "Abe", "foto": "images/Kandidat6.jpg"},
     {"nama": "Yogi", "foto": "images/Kandidat7.jpg"},
